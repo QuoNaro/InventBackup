bc0d0e51522ad204f159476f531fef3203edbcb0
quonaro <quonaro@mail.ru>
2024-09-03 14:54:38 +0700

    add autoremove backups

--- source/scripts/backup.sh
+++ source/scripts/backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
-
 while true; do
     echo "---------------------------------------------------"
+    # Удаляем файлы старше 6 месяцев
+    find "/app/BACKUP/" -type f -mtime +180 -exec rm -f {} \;
     bash /app/backup-now.sh
     echo "---------------------------------------------------"
-    sleep 86400  # 86400 секунд = 24 часа
+    sleep $TIMEOUT
 done
 
