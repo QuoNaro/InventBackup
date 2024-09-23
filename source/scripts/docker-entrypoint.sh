87020e1ec7ffdc601c8cb43d73449f04a88c3a58
quonaro <quonaro@mail.ru>
2024-09-23 12:05:56 +0000

    fix

--- source/scripts/docker-entrypoint.sh
+++ source/scripts/docker-entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-# Обновление и установка пакетов
-apt-get update -qq &&
-apt-get install -qq -y sshpass xz-utils cron nano && 
-mkdir -p ./backup/ ./logs && 
-
-# Установка прав доступа
-chmod +x /app/test.sh /app/backup.sh /app/cleanup.sh
-
-echo "0 0 * * * /app/backup.sh >> /app/logs/cron.log 2>&1" >> /etc/crontab &&
-echo "0 1 1 * * /app/cleanup.sh >> /app/logs/cron.log 2>&1" >> /etc/crontab &&
-echo "*/2 * * * * /app/test.sh >> /app/logs/test.log 2>&1" >> /etc/crontab &&
-crontab /etc/crontab
-
-
 # Выполнить backup.sh
 sh /app/backup.sh >> /app/logs/backup.log 2>&1
 
